b248e5a9fe8be405b8385b97c72776c4d5770394
popInNotOut <randomzero1234@gmail.com>
2025-08-31 20:28:03 -0400

    Refactored code to make game UI-based

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-/* BEGINNING FUNCTIONS */
 function getComputerChoice(){
     let randNum = Math.random();
     if (0 <= randNum && randNum <= 1/3){
@@ -12,57 +11,59 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let ret = prompt("rock, paper, or scissors: ");
-    while (ret.toLowerCase() != "rock" && ret.toLowerCase() != "paper" && ret.toLowerCase() != "scissors"){
-        ret = prompt("Invalid input. Please type rock, paper, or scissors: ");
-    }
-    return ret;
-}
-
 function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase();
+    const output = document.querySelector("#output");
+    const humanPoints = document.querySelector("#human");
+    const computerPoints = document.querySelector("#computer");
+    const gameResult = document.querySelector("#game-result");
+
+    output.textContent = "You selected: " + humanChoice + ", computer selected: " + computerChoice;
+
     if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "scissors" && computerChoice == "paper" || humanChoice == "paper" && computerChoice == "rock"){
         humanScore++;
-        alert("You selected: " + humanChoice + ", computer selected: " + computerChoice + ". Therefore, you win");
+        humanPoints.textContent = "Your points: " + humanScore;
     }
     else if (humanChoice == "rock" && computerChoice == "paper" || humanChoice == "scissors" && computerChoice == "rock" || humanChoice == "paper" && computerChoice == "scissors"){
         computerScore++;
-        alert("You selected: " + humanChoice + ", computer selected: " + computerChoice + ". Therefore, you lose");
+        computerPoints.textContent = "Computer's points: " + computerScore;
     }
-    else {
-        alert("You selected: " + humanChoice + ", computer selected: " + computerChoice + ". Therefore, draw");
-    }
-    alert("Your score: " + humanScore + ", computer score: " + computerScore + ", rounds remaining: " + rounds);
-}
 
-function playGame(){
-    alert("You will play 5 rounds of rock paper scissors.");
-    while (rounds-- > 0){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
+    if (humanScore == ROUNDS_WIN_CONDITION){
+        gameResult.textContent = "You win!";
+        gameResult.style.fontWeight = "bold";
     }
-    if (humanScore > computerScore){
-        alert("You win!");
-    }
-    else if (humanScore == computerScore){
-        alert("Draw!");
-    }
-    else {
-        alert("You lose!");
+    else if (computerScore ==  ROUNDS_WIN_CONDITION){
+        gameResult.textContent = "You lose!";
+        gameResult.style.fontWeight = "bold";
     }
 }
-/* END OF FUNCTIONS */
 
+let humanScore = 0, computerScore = 0;
+const ROUNDS_WIN_CONDITION = 5;
 
-
-/* START OF VARIABLES */
-let humanScore = 0, computerScore = 0, rounds = 5;
-/* END OF VARIABLES */
-
-
-
-/* START OF MAIN CODE */
-playGame();
-/* END OF MAIN CODE */
+let selectList = document.querySelector("#selection");
+selectList.addEventListener("click", (event)=>{
+    if (humanScore == ROUNDS_WIN_CONDITION || computerScore == ROUNDS_WIN_CONDITION){
+        alert("Game is over");
+        return;
+    }
+    let target = event.target;
+    console.log(target);
+    /*
+    switch(target.id){
+        case "rock":
+            playRound("rock", getComputerChoice());
+            break;
+        case "paper":
+            playRound("paper", getComputerChoice());
+            break;
+        case "scissors":
+            playRound("scissors", getComputerChoice());
+            break;
+        default:
+            alert("Error");
+            break;
+    }
+    */
+   playRound(target.id, getComputerChoice());
+});
